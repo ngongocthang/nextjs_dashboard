91e2289f32cd4147be7db06f1526ad53d8966538
ngongocthang <ngongocthang18082004@gmail.com>
2024-04-26 14:34:47 +0700

    commit two

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,8 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${invoice.name}'s profile picture`} // Delete this line
+
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -90,7 +91,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${invoice.name}'s profile picture`} // Delete this line
                       />
                       <p>{invoice.name}</p>
                     </div>
